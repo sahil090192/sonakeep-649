a71d5a65a66934f09db1fda0f9e98ef0d3879e13
Sahil Aggarwal <sahil090192@gmail.com>
2026-04-24 21:30:27 -0500

    Refresh Pages with Riviera design

--- expo/metro.config.js
+++ expo/metro.config.js
@@ -1,6 +1,13 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withRorkMetro } = require("@rork-ai/toolkit-sdk/metro");
 
 const config = getDefaultConfig(__dirname);
+
+let withRorkMetro = (metroConfig) => metroConfig;
+
+try {
+  ({ withRorkMetro } = require("@rork-ai/toolkit-sdk/metro"));
+} catch {
+  console.warn("[metro] @rork-ai/toolkit-sdk not installed; using Expo default Metro config.");
+}
 
 module.exports = withRorkMetro(config);
